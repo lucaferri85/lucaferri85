64e70ebe3b18064c76b0102e79a7cd7c79ac54c2
Luca <159878033+lucaferri85@users.noreply.github.com>
2026-09-12 14:21:07 +0200

    Fix Electron routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import "@/App.css";
 import "@/index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Workspace from "@/pages/Workspace";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Workspace />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
